63ece15fb13a80ec0de644f923c07b8476c80eaf
Tanvir Sijan <mdtanvirhossain.sijan@my.jcu.edu.au>
2025-02-09 20:50:07 +1000

    refactor password

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,10 +2,17 @@ def main():
     min_length = 8
 
     while True:
-        password = input('Enter a password: ')
+        password = get_password()
         if len(password) < min_length:
             break
         print("Password too short!")
     print("*"*len(password))
+
+
+def get_password():
+    password = input('Enter a password: ')
+    return password
+
+
 if __name__ == '__main__':
     main()
