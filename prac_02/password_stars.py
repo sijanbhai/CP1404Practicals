8af4f87e9db4da55ebcb7881cd0f525da309fbc7
Tanvir Sijan <mdtanvirhossain.sijan@my.jcu.edu.au>
2025-02-09 20:46:53 +1000

    Add main function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,11 @@
-min_length = 8
+def main():
+    min_length = 8
 
-while True:
-    password = input('Enter a password: ')
-    if len(password) < min_length:
-        break
-    print("Password too short!")
-print("*"*len(password))
+    while True:
+        password = input('Enter a password: ')
+        if len(password) < min_length:
+            break
+        print("Password too short!")
+    print("*"*len(password))
+if __name__ == '__main__':
+    main()
