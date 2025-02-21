d4e241337d09bd8bbc99ee10610ff5c5ac68e84f
Tanvir Sijan <mdtanvirhossain.sijan@my.jcu.edu.au>
2025-02-21 12:07:44 +1000

    Add file reading and writing for user input and number calculations

---
+++ prac_03/files.py
@@ -0,0 +1,32 @@
+"""
+CP1404/CP5632 - Practical
+Reads and writes data to files, including user input and numeric calculations.
+"""
+
+
+
+# Task 1
+name = input("What is your name? ")
+file = open('name.txt', 'w')
+file.write(name)
+file.close()
+
+# Task 2
+file = open('name.txt', 'r')
+name_from_file = file.readline().strip()
+file.close()
+print(f"Hi {name_from_file}!")
+
+# Task 3
+with open('numbers.txt', 'r') as file:
+    first_number = int(file.readline().strip())
+    second_number = int(file.readline().strip())
+    result = first_number + second_number
+    print(result)
+
+# Task 4
+with open('numbers.txt', 'r') as file:
+    total = 0
+    for line in file:
+        total += int(line.strip())
+    print(total)
