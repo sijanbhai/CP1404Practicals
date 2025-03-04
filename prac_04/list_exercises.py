28d513a28d3c35c1448e58aad3784ec9c8877c19
Tanvir Sijan <mdtanvirhossain.sijan@my.jcu.edu.au>
2025-03-04 17:21:08 +1000

    Add a program to collect 5 numbers

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,20 @@
+"""
+CP1404/CP5632 Practical
+List Exercises
+"""
+def main():
+    """Prompt the user for 5 numbers, store them in a list, and display statistics."""
+    numbers = []  # Initialize an empty list
+
+    for i in range(5):
+        number = float(input(f"Number: "))  # Get user input
+        numbers.append(number)  # Add the number to the list
+
+    print("\nThe first number is", numbers[0])
+    print("The last number is", numbers[-1])
+    print("The smallest number is", min(numbers))
+    print("The largest number is", max(numbers))
+    print("The average of the numbers is", sum(numbers) / len(numbers))
+
+main()
+
