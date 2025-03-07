32842ea47214864c88a6f4ba8e2ffc3b95f1a796
Tanvir Sijan <mdtanvirhossain.sijan@my.jcu.edu.au>
2025-03-07 14:23:05 +1000

    Add loop to print all states

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -7,7 +7,9 @@ File needs reformatting
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
-print(CODE_TO_NAME)
+
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:3} is {name}")
 
 state_code = input("Enter short state: ")
 while state_code != "":
